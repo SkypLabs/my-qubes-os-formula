262dade13ef6259db9dbcaae9f13d7fab0b720fc
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2019-02-21 08:26:33 +0000

    Fix 'copy-from-vm-to-dom0.sh'

--- copy-from-vm-to-dom0.sh
+++ copy-from-vm-to-dom0.sh
@@ -4,21 +4,21 @@
 # --------------------------------------------------------------#
 
 DIR_PREFIX=/srv/salt
-VM_TARGET=
-DIR_TARGET=
+VM_TARGET=personal-dev
+DIR_TARGET=/home/user/Documents/my-qubes-os-formula
 
 # Script
 # --------------------------------------------------------------#
 
 qvm-run --pass-io ${VM_TARGET} "cat ${DIR_TARGET}/my-config.top" > ${DIR_PREFIX}/my-config.top
 
-directories=$(qvm-run --pass-io ${VM_TARGET} "find ${DIR_TARGET}/my-config -type d")
-files=$(qvm-run --pass-io ${VM_TARGET} "find ${DIR_TARGET}/my-config -type f")
+directories=$(qvm-run --pass-io ${VM_TARGET} "find ${DIR_TARGET}/my-config -type d -printf '%P\n'")
+files=$(qvm-run --pass-io ${VM_TARGET} "find ${DIR_TARGET}/my-config -type f -printf '%P\n'")
 
 for dir in ${directories}; do
-  mkdir -p "${dir}"
+  mkdir -p "${DIR_PREFIX}/my-config/${dir}"
 done
 
 for file in ${files}; do
-  qvm-run --pass-io ${VM_TARGET} "cat ${file}" > "${file}"
+  qvm-run --pass-io ${VM_TARGET} "cat ${DIR_TARGET}/my-config/${file}" > "${DIR_PREFIX}/my-config/${file}"
 done
